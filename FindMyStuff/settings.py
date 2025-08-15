ceae0fbcea6605f2da765b5aeda6088b46fac0e5
shihab-7 <shihabshahriar543@gmail.com>
2025-08-15 06:11:06 +0600

    fixing path issue

--- FindMyStuff/settings.py
+++ FindMyStuff/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Additional static files directories
 STATICFILES_DIRS = [
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 # Static files collection directory (for production)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
